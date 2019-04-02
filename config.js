70f596a3ca729ab8d3ab9219352ee973257676bb
LazaroMelo <lazaromelojr@gmail.com>
2019-04-02 14:46:54 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'lm-tecnologia',
+		wp_ws_url : 'http://lazaromelojr.com.br/portal/wp-appkit-api/lm-tecnologia',
+		wp_url : 'http://lazaromelojr.com.br/portal',
+		theme : 'q-android',
+		version : '0.1',
+		app_type : 'phonegap-build',
+		app_title : 'LM-Tecnologia',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : -3,
+		debug_mode : 'off',
+		auth_key : 'eOZ*$!aPb!*qiu5L5pWaQY,.Cd*:UF|hWHTC~ei:5{k<8Ji+kpv* {JvRgh!jC95',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
